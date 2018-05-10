0eefa30af947f6169127110b8057121253de2b4e
rjz <rj@rjzaworski.com>
2018-05-09 21:21:31 -0700

    Expands jumbo elements

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
   parser: 'postcss-scss',
   plugins: [
     require('precss')(),
+    require('postcss-calc')(),
     require('postcss-import')({
       paths: [
         'node_modules/normalize.css',
